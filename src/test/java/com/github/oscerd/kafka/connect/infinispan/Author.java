60ba20f4e21ccd8c7e2a9f41d69d4c6ccc6ec8b5
Andrea Cosentino <ancosen@gmail.com>
2017-05-24 10:54:30 +0200

    First Integration test

---
+++ src/test/java/com/github/oscerd/kafka/connect/infinispan/Author.java
@@ -0,0 +1,29 @@
+package com.github.oscerd.kafka.connect.infinispan;
+
+import java.io.Serializable;
+
+import org.infinispan.protostream.annotations.ProtoDoc;
+import org.infinispan.protostream.annotations.ProtoField;
+
+@ProtoDoc("@Indexed")
+public class Author implements Serializable {
+	
+	private String name;
+
+	@ProtoField(number = 1, required = true)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Author [name=" + name + "]";
+	}
+	
+	
+
+}
---
+++ src/test/java/com/github/oscerd/kafka/connect/infinispan/InfinispanTaskTestIT.java
@@ -0,0 +1,93 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.oscerd.kafka.connect.infinispan;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilderException;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class InfinispanTaskTestIT {
+
+	@Test
+	public void test() throws JsonProcessingException {
+		Map<String, String> props = new HashMap<>();
+		props.put("infinispan.connection.hosts", "127.0.0.1");
+		props.put("infinispan.connection.hotrod.port", "11222");
+		props.put("infinispan.connection.cache.name", "default");
+		props.put("infinispan.cache.force.return.values", "true");
+		props.put("infinispan.cache.maxidle.default", "false");
+		props.put("infinispan.cache.lifespan.default", "false");
+		props.put("infinispan.use.proto", "true");
+		props.put("infinispan.proto.marshaller.class", "com.github.oscerd.kafka.connect.infinispan.Author");
+
+		InfinispanSinkTask infinispanSinkTask = new InfinispanSinkTask();
+		infinispanSinkTask.start(props);
+
+		final String topic = "atopic";
+
+		Author author = new Author();
+		author.setName("author");
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		infinispanSinkTask.put(Collections
+				.singleton(new SinkRecord(topic, 1, null, "author", null, mapper.writeValueAsString(author), 42)));
+		
+        ConfigurationBuilder builder = new ConfigurationBuilder()
+                .addServer()
+                .host("localhost")
+                .port(11222)
+                .marshaller(new ProtoStreamMarshaller());
+
+         RemoteCacheManager cacheManager = new RemoteCacheManager(builder.build());
+         
+			SerializationContext serCtx = ProtoStreamMarshaller.getSerializationContext(cacheManager);
+			ProtoSchemaBuilder protoSchemaBuilder = new ProtoSchemaBuilder();
+			String memoSchemaFile = null;
+			try {
+				memoSchemaFile = protoSchemaBuilder
+						.fileName("file.proto")
+						.packageName("test")
+						.addClass(Author.class)
+						.build(serCtx);
+			} catch (ProtoSchemaBuilderException | IOException e) {
+				e.printStackTrace();
+			}
+
+			// register the schemas with the server too
+			RemoteCache<String, String> metadataCache = cacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+			metadataCache.put("file.proto", memoSchemaFile);
+		    RemoteCache<Object, Object> cache = cacheManager.getCache("default");
+		    
+		    org.junit.Assert.assertEquals(cache.get("author").toString(), author.toString());
+	}
+}
